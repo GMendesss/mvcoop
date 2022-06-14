ed320f7a56126088385bfa488368c41d541a0a94
GMendesss <estacazero123@gmail.com>
2022-06-14 10:53:16 -0300

    Primeiro commit

---
+++ model/init.php
@@ -0,0 +1,10 @@
+<?php
+//timezone
+date_default_timezone_set('America/Sao_Paulo');
+
+//conexão com banco de dados
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','id18902137_gmendes');
+define('BD_SENHA','Xd8$R#aUErEL$4$}');
+define('BD_BANCO','id18902137_livraria');
+?>
